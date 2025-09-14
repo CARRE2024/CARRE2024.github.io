68fb8ae218c84ddba343724d01179076d945fe37
CARRE2024 <visoroptimizado@gmail.com>
2025-09-14 13:49:03 +0200

    Add files via upload

---
+++ musica.js
@@ -0,0 +1,21 @@
+// Título de la canción
+window.songTitle = "Clocks - Coldplay";
+
+// Melodía (intro simplificada en C menor, arpegio)
+// C#4 - G#4 - C5 - G#4  (repetido en patrón)
+window.melody = [
+  61,68,72,68,   // Db4 - Ab4 - C5 - Ab4
+  61,68,72,68,
+  61,68,72,68,
+  61,68,72,68,
+
+  63,70,75,70,   // Eb4 - Bb4 - D5 - Bb4
+  63,70,75,70,
+  63,70,75,70,
+  63,70,75,70,
+
+  58,65,70,65,   // A#3 - F4 - Bb4 - F4 (variación)
+  58,65,70,65,
+  58,65,70,65,
+  58,65,70,65
+];
